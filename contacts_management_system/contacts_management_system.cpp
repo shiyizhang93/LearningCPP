69b26760833979f115b9d4f97e8c2f306bd2f240
shiyi <shiyizhang93@gmail.com>
2022-05-05 17:03:34 +0800

    Finish show menu function

---
+++ contacts_management_system/contacts_management_system.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Shiyi on 2022/4/29.
+//
+
+#include <iostream>
+
+
+void ShowMenu()
+{
+    std::cout << "**************************" << std::endl;
+    std::cout << "*** 1. Add contact *******" << std::endl;
+    std::cout << "*** 2. Show contacts *****" << std::endl;
+    std::cout << "*** 3. Delete contact ****" << std::endl;
+    std::cout << "*** 4. Search contact ****" << std::endl;
+    std::cout << "*** 5. Modify contact ****" << std::endl;
+    std::cout << "*** 6. Empty contacts ****" << std::endl;
+    std::cout << "*** 0. Quit system *******" << std::endl;
+    std::cout << "**************************" << std::endl;
+}
---
+++ contacts_management_system/contacts_management_system.h
@@ -0,0 +1,17 @@
+//
+// Created by Shiyi on 2022/5/5.
+//
+
+#include <iostream>
+
+#ifndef LEARNINGC_CONTACTS_MANAGEMENT_SYSTEM_H
+#define LEARNINGC_CONTACTS_MANAGEMENT_SYSTEM_H
+
+/*
+ * Gives some user operation hints. It offers 5 different operations.
+ */
+void ShowMenu();
+
+
+
+#endif //LEARNINGC_CONTACTS_MANAGEMENT_SYSTEM_H
