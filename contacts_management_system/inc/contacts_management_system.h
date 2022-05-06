cda9b190b8e962ec67b69776a0b067a799acea7d
shiyi <shiyizhang93@gmail.com>
2022-05-06 17:22:19 +0800

    Finish quit-system and add-contact functions

--- contacts_management_system/inc/contacts_management_system.h
+++ contacts_management_system/inc/contacts_management_system.h
@@ -3,15 +3,35 @@
 //
 
 #include <iostream>
+#include <string>
+
+#define MAX 1000
 
 #ifndef LEARNINGC_CONTACTS_MANAGEMENT_SYSTEM_H
 #define LEARNINGC_CONTACTS_MANAGEMENT_SYSTEM_H
+
+// declare person info struct
+struct Person
+{
+    std::string name;
+    int sex;
+    int age;
+    std::string phone_num;
+    std::string addr;
+};
+
+// declare contacts-book info struct
+// member variables are contacts array and the current number of contacts
+struct ContactsBook
+{
+    struct Person contacts_array[MAX];
+    int size;
+};
 
 /*
  * Gives some user operation hints. It offers 5 different operations.
  */
 void ShowMenu();
-
 
 
 #endif //LEARNINGC_CONTACTS_MANAGEMENT_SYSTEM_H
--- contacts_management_system/src/main.cpp
+++ contacts_management_system/src/main.cpp
@@ -3,9 +3,47 @@
 //
 
 #include <iostream>
-#include "inc/contacts_management_system.h"
+#include "contacts_management_system.h"
 
 int main()
 {
+    // init user input variable
+    int user_input = 0;
 
+    while(1)
+    {
+        // invoke ShowMenu function
+        ShowMenu();
+
+        std::cin >> user_input;
+
+        switch(user_input)
+        {
+            // add contact
+            case 1:
+                break;
+            // show contacts
+            case 2:
+                break;
+            // delete contact
+            case 3:
+                break;
+            // search contact
+            case 4:
+                break;
+            // modify contact
+            case 5:
+                break;
+            // empty contacts
+            case 6:
+                break;
+            // quit system
+            case 0:
+                std::cout << "Welcome to use next time!" << std::endl;
+                system("pause");
+                return 0;
+            default:
+                break;
+        }
+    }
 }
